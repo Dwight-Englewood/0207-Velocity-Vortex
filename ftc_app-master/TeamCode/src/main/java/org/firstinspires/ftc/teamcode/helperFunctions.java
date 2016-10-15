322b78d7dfd8d8cc72cbfe2ddc77a0177d084b61
Burtorustum <aburur@d-e.org>
2016-10-14 21:14:25 -0400

    Started work on the helper functions, this robstestiterative is what was used on 10/14/16

--- ftc_app-master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/helperFunctions.java
+++ ftc_app-master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/helperFunctions.java
@@ -17,4 +17,13 @@ public class helperFunctions {
         double wheelSize = 10;  //TODO: This should be changed to the circumference of the wheel, 10 is currently just a filler
         return (distance / wheelSize);
     }
+    public static double increaseToOne (double input)
+    {
+        //turns inputs of less than 1 into inputs of one, as long as they are greater than 0
+        if (input > 0) {
+            return 1.0;
+        } else {
+            return 0.0;
+        }
+    }
 }
