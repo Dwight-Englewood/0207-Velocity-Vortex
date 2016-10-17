c482d675fd1620178ac216e871fa7bab49c741d7
Burtorustum <aburur@d-e.org>
2016-10-17 09:53:48 -0400

    fixed a few more comments

--- ftc_app-master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/helperFunctions.java
+++ ftc_app-master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/helperFunctions.java
@@ -6,7 +6,7 @@ package org.firstinspires.ftc.teamcode;
 
 public class helperFunctions {
     public static double buttonToPower (boolean isDown) {
-        /*This function was designed for the R2 and L2 bumpers (or other buttons). Those buttons return a boolean for down or up.
+        /*This function was designed for the right and left bumpers but can be used on any button that returns a boolean.
          This function takes a boolean and returns full power or no power, for true and false, respectively
         */
           if (isDown) {
@@ -23,7 +23,7 @@ public class helperFunctions {
 
     public static double triggerToFlat (double input)
     {
-        /*This function was designed for the R1 and L1 triggers. Those buttons return doubles; however, we just want it to be full power or off.
+        /*This function was designed for the Right and Left triggers. Those buttons return doubles; however, we just want it to be full power or off.
         This function will take a double, and turn it to full power or no power, depending on the range it is in
         */
         if (input > 0.1) {//0.1, since that will prevent accidental triggers
