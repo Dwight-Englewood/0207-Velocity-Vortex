829f01bd67c0dcd2120b61d8a9ad1e25b67ef95b
weznon <waterwasp55555@gmail.com>
2016-10-19 14:17:01 +0000

    ayy

--- ftc_app-master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OFFICIAL_PROGRAM.java
+++ ftc_app-master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OFFICIAL_PROGRAM.java
@@ -41,20 +41,6 @@ import com.qualcomm.robotcore.hardware.Servo;
 import com.qualcomm.robotcore.util.ElapsedTime;
 import org.firstinspires.ftc.teamcode.helperFunctions;
 
-/**
- * This file contains an example of an iterative (Non-Linear) "OpMode".
- * An OpMode is a 'program' that runs in either the autonomous or the UselessTeleop period of an FTC match.
- * The names of OpModes appear on the menu of the FTC Driver Station.
- * When an selection is made from the menu, the corresponding OpMode
- * class is instantiated on the Robot Controller and executed.
- *
- * This particular OpMode just executes a basic Tank Drive Teleop for a PushBot
- * It includes all the skeletal structure that all iterative OpModes contain.
- *
- * Use Android Studios to Copy this Class, and Paste it into your team's code folder with a new name.
- * Remove or comment out the @Disabled line to add this opmode to the Driver Station OpMode list
- */
-
 @TeleOp(name="OFFICIAL_PROGRAM", group="Iterative Opmode")  // @Autonomous(...) is the other common choice
 //@Disabled
 public class OFFICIAL_PROGRAM extends OpMode
@@ -65,7 +51,7 @@ public class OFFICIAL_PROGRAM extends OpMode
     private DcMotor leftMotor = null;
     private DcMotor rightMotor = null;
     private DcMotor elevator = null;
-    //zprivate DcMotor shooter = null;
+    //private DcMotor shooter = null;
 
     //private Servo rightPoker = null;
     //private Servo leftPoker = null;
