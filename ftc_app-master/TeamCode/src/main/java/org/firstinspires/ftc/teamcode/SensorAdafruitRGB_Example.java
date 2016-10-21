7fede126d7ffff2fab8b89c4e4699d9f07a26e39
Burtorustum <aburur@d-e.org>
2016-10-21 17:20:26 -0400

    Cleaned directory of trash. Added Sensor EXAMPLE.

--- ftc_app-master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/SensorAdafruitRGB_Example.java
+++ ftc_app-master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/SensorAdafruitRGB_Example.java
@@ -70,9 +70,9 @@ import com.qualcomm.robotcore.hardware.DigitalChannelController;
  * Use Android Studio to Copy this Class, and Paste it into your team's code folder with a new name.
  * Remove or comment out the @Disabled line to add this opmode to the Driver Station OpMode list
  */
-@Autonomous(name = "RobsTestAuton", group = "Sensor")
-//@Disabled                            // Comment this out to add to the opmode list
-public class RobsTestAuton extends LinearOpMode {
+@Autonomous(name = "Sensor: AdafruitRGB", group = "Sensor")
+@Disabled                            // Comment this out to add to the opmode list
+public class SensorAdafruitRGB_Example extends LinearOpMode {
 
   ColorSensor sensorRGB;
   DeviceInterfaceModule cdim;
