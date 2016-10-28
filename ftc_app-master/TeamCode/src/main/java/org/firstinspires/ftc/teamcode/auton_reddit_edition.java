1dd158b6430cb5b0e9fc488fa1bf00f0de13d5c8
Wen <weznon@localhost.localdomain>
2016-10-28 18:55:29 -0400

    asd

--- ftc_app-master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/auton_reddit_edition.java
+++ ftc_app-master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/auton_reddit_edition.java
@@ -6,8 +6,8 @@ import com.qualcomm.robotcore.hardware.DcMotor;
 
 public class auton_reddit_edition extends OpMode {
 
-    DcMotor motorRight;
-    DcMotor motorLeft;
+    DcMotor rightMotor;
+    DcMotor leftMotor;
     DcMotor elevator = null;
     DcMotor shooter = null;
 
@@ -32,13 +32,26 @@ public class auton_reddit_edition extends OpMode {
 
     @Override
     public void loop() {
-        float powerlevel = 0.0f;
-
+        int newTargetL = leftMotor.getCurrentPosition() + 7;
+        int newTargetR = RightMotor.getCurrentPosition() + 7;
+        
+        leftMotor.setTargetPosition(newTargetL);
+        rightMotor.setTargetPosition(newTargetR);
+        leftMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        rightMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        leftMotor.setPower(.5);
+        rightMotor.setPower(.5);
+        //while motor is busy add random stuff to telemetry?
+          //float powerlevel = 0.0f;
+  
         // If we're still with the first 3 seconds after pressing start keep driving forward
-        if (System.currentTimeMillis() < start_time + 3000) {
-            powerlevel = 0.5f;
-        }
-        motorLeft.setPower(powerlevel);
-        motorRight.setPower(powerlevel);
+        //if (System.currentTimeMillis() < start_time + 3000) {
+           // powerlevel = 0.5f;
+        //}
+        //motorLeft.setPower(powerlevel);
+        //motorRight.setPower(powerlevel);
+        leftMotor.setPower(0);
+        rightMotor.setPower(0);
+
     }
 }
