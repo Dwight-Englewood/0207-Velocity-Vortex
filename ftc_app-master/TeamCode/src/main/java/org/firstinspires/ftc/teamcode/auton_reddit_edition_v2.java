607a61500bb68ca857dc7a61ca60ff108cfcb587
weznon <waterwasp55555@gmail.com>
2016-10-29 01:33:55 +0000

    possible fix for encoders? probably not though

--- ftc_app-master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/auton_reddit_edition_v2.java
+++ ftc_app-master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/auton_reddit_edition_v2.java
@@ -7,9 +7,8 @@ import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.hardware.DcMotor;
 
 
-
-@Autonomous(name = "redditlul", group = "LINEAR_AUTON")
-public class auton_reddit_edition extends OpMode {
+@Autonomous(name = "redditlullul", group = "LINEAR_AUTON")
+public class auton_reddit_edition_v2 extends OpMode {
 
     DcMotor rightMotor;
     DcMotor leftMotor;
@@ -20,6 +19,7 @@ public class auton_reddit_edition extends OpMode {
     long start_time;
     int i;
 
+
     @Override
     public void init() {
         leftMotor = hardwareMap.dcMotor.get("left motor");
@@ -27,20 +27,21 @@ public class auton_reddit_edition extends OpMode {
         elevator = hardwareMap.dcMotor.get("elevator");
         shooter = hardwareMap.dcMotor.get("shooter");
 
+        leftMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        rightMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+
         leftMotor.setDirection(DcMotor.Direction.REVERSE);
         rightMotor.setDirection(DcMotor.Direction.REVERSE);
     }
 
     @Override
     public void start() {
-        super.start();
-        // Save the system clock when start is pressed
-        start_time = System.currentTimeMillis();
     }
 
     @Override
     public void loop() {
         //Gets stuck in infinite loop? Not sure what's happening, though I suspect it's because the enconders arent in fully
+        //So it can't read the isBusy, and never will end.
 
         newTargetL = leftMotor.getCurrentPosition() + 7;
         newTargetR = rightMotor.getCurrentPosition() + 7;
@@ -48,9 +49,6 @@ public class auton_reddit_edition extends OpMode {
 
         leftMotor.setTargetPosition(newTargetL);
         rightMotor.setTargetPosition(newTargetR);
-
-        leftMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-        rightMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
 
         leftMotor.setPower(.5);
         rightMotor.setPower(.5);
@@ -71,10 +69,5 @@ public class auton_reddit_edition extends OpMode {
         //motorLeft.setPower(powerlevel);
         //motorRight.setPower(powerlevel);
 
-        leftMotor.setPower(0);
-        rightMotor.setPower(0);
-
-        leftMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-        rightMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
     }
 }
