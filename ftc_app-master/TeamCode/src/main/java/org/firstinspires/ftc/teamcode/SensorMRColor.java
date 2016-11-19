333d09d07246cde887ab0a36343a629eeb0176f4
Burtorustum <aburur@d-e.org>
2016-11-19 17:47:51 -0500

    reset the MR COLOR default file for regular testing

--- ftc_app-master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/SensorMRColor.java
+++ ftc_app-master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/SensorMRColor.java
@@ -55,10 +55,11 @@ import com.qualcomm.robotcore.hardware.ColorSensor;
  * Remove or comment out the @Disabled line to add this opmode to the Driver Station OpMode list
  */
 @TeleOp(name = "Sensor: MR Color", group = "Sensor")
-//@Disabled
+@Disabled
 public class SensorMRColor extends LinearOpMode {
 
   ColorSensor colorSensor;    // Hardware Device Object
+
 
   @Override
   public void runOpMode() throws InterruptedException {
@@ -73,11 +74,18 @@ public class SensorMRColor extends LinearOpMode {
     // color of the Robot Controller app to match the hue detected by the RGB sensor.
     final View relativeLayout = ((Activity) hardwareMap.appContext).findViewById(R.id.RelativeLayout);
 
+    // bPrevState and bCurrState represent the previous and current state of the button.
+    boolean bPrevState = false;
+    boolean bCurrState = false;
+
+    // bLedOn represents the state of the LED.
+    boolean bLedOn = true;
+
     // get a reference to our ColorSensor object.
     colorSensor = hardwareMap.colorSensor.get("color sensor");
 
     // Set the LED in the beginning
-    colorSensor.enableLed(true);
+    colorSensor.enableLed(bLedOn);
 
     // wait for the start button to be pressed.
     waitForStart();
@@ -86,11 +94,25 @@ public class SensorMRColor extends LinearOpMode {
     // Note we use opModeIsActive() as our loop condition because it is an interruptible method.
     while (opModeIsActive()) {
 
+      // check the status of the x button on either gamepad.
+      bCurrState = gamepad1.x;
+
+      // check for button state transitions.
+      if ((bCurrState == true) && (bCurrState != bPrevState))  {
+
+        // button is transitioning to a pressed state. So Toggle LED
+        bLedOn = !bLedOn;
+        colorSensor.enableLed(bLedOn);
+      }
+
+      // update previous state variable.
+      bPrevState = bCurrState;
 
       // convert the RGB values to HSV values.
       Color.RGBToHSV(colorSensor.red() * 8, colorSensor.green() * 8, colorSensor.blue() * 8, hsvValues);
 
       // send the info back to driver station using telemetry function.
+      telemetry.addData("LED", bLedOn ? "On" : "Off");
       telemetry.addData("Clear", colorSensor.alpha());
       telemetry.addData("Red  ", colorSensor.red());
       telemetry.addData("Green", colorSensor.green());
