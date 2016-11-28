af531e092e2f06c18eb35bb927309f473a6dd412
Burtorustum <aburur@d-e.org>
2016-11-28 10:24:12 -0500

    idk

---
+++ ftc_app-master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/encoderAutonRGBBlue.java
@@ -0,0 +1,91 @@
+package org.firstinspires.ftc.teamcode;
+
+/*plotnw*/
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.hardware.ColorSensor;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.Servo;
+
+//@Disabled
+@Autonomous(name = "encoderAutonRGBBlue", group = "ITERATIVE_AUTON")
+public class encoderAutonRGBBlue extends OpMode {
+
+    long start_time;
+    long current_time;
+    long wait_time;
+    long time;
+
+    DcMotor rightMotor = null;
+    DcMotor leftMotor = null;
+    DcMotor elevator = null;
+    DcMotor shooter = null;
+    Servo poker = null;
+
+    private double startPos = 0.48;
+    private double currentPos = startPos;
+    private double maxPos = 0.78;
+    private double minPos = 0.18;
+
+    ColorSensor colorSensor;
+    boolean bLedOn = false;
+
+    final double[] distanceArray = {0.0, 0.0};
+
+    @Override
+    public void init() {
+
+        poker = hardwareMap.servo.get("poker");
+        poker.setPosition(startPos);
+
+        colorSensor = hardwareMap.colorSensor.get("color sensor");
+        colorSensor.enableLed(bLedOn);
+
+        leftMotor = hardwareMap.dcMotor.get("left motor");
+        rightMotor = hardwareMap.dcMotor.get("right motor");
+        elevator = hardwareMap.dcMotor.get("elevator");
+        shooter = hardwareMap.dcMotor.get("shooter");
+
+        leftMotor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        rightMotor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+
+        leftMotor.setDirection(DcMotor.Direction.REVERSE);
+        rightMotor.setDirection(DcMotor.Direction.FORWARD);
+        elevator.setDirection(DcMotor.Direction.FORWARD);
+
+    }
+
+    @Override
+    public void start() {
+        super.start();
+        // Save the system clock when start is pressed
+        start_time = System.currentTimeMillis();
+    }
+
+    @Override
+    public void loop() {
+        current_time = System.currentTimeMillis();
+        time = current_time - start_time;
+
+        if (!leftMotor.isBusy() && !rightMotor.isBusy())
+        {
+
+
+
+        }
+
+
+        telemetry.addData("LED", bLedOn ? "On" : "Off");
+        telemetry.addData("Clear", colorSensor.alpha());
+        telemetry.addData("Red  ", colorSensor.red());
+        telemetry.addData("Blue ", colorSensor.blue());
+        telemetry.update();
+
+    }
+    @Override
+    public void stop() {
+        poker.setPosition(startPos);
+    }
+}
