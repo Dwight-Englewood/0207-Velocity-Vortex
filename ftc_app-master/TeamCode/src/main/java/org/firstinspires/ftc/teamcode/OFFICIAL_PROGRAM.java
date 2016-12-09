c9d837716303e0bf135c16d7305b7258b2600a06
Weznon <waterwasp55555@gmail.com>
2016-12-09 13:12:42 -0500

    fixed brackets, added some comments. needs testing for crservo still

--- ftc_app-master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OFFICIAL_PROGRAM.java
+++ ftc_app-master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OFFICIAL_PROGRAM.java
@@ -1,87 +1,47 @@
-
-/*
-Copyright (c) 2016 Robert Atkinson
-
-All rights reserved.
-
-Redistribution and use in source and binary forms, with or without modification,
-are permitted (subject to the limitations in the disclaimer below) provided that
-the following conditions are met:
-
-Redistributions of source code must retain the above copyright notice, this list
-of conditions and the following disclaimer.
-
-Redistributions in binary form must reproduce the above copyright notice, this
-list of conditions and the following disclaimer in the documentation and/or
-other materials provided with the distribution.
-
-Neither the name of Robert Atkinson nor the names of his contributors may be used to
-endorse or promote products derived from this software without specific prior
-written permission.
-
-NO EXPRESS OR IMPLIED LICENSES TO ANY PARTY'S PATENT RIGHTS ARE GRANTED BY THIS
-LICENSE. THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
-"AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
-THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESSFOR A PARTICULAR PURPOSE
-ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE
-FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
-DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
-SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
-CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR
-TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
-THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
-*/
 package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
-import com.qualcomm.robotcore.hardware.CRServo;
-import com.qualcomm.robotcore.hardware.ColorSensor;
 import com.qualcomm.robotcore.hardware.DcMotor;
-import com.qualcomm.robotcore.hardware.Gamepad;
 import com.qualcomm.robotcore.hardware.Servo;
 import com.qualcomm.robotcore.util.ElapsedTime;
 
-@TeleOp(name="TELEBOP", group="Iterative Opmode")  // @Autonomous(...) is the other common choice
+@TeleOp(name="TELEBOP", group="Iterative Opmode")
 //@Disabled
-public class OFFICIAL_PROGRAM extends OpMode
-{
+public class OFFICIAL_PROGRAM extends OpMode {
     /* Declare OpMode members. */
     private ElapsedTime runtime = new ElapsedTime();
-
+    //Defining Motors/Servos
     private DcMotor leftMotor = null;
     private DcMotor rightMotor = null;
     private DcMotor elevator = null;
     private DcMotor shooter = null;
-
     private Servo poker = null;
     private Servo pokerWheel = null;
+
+    //Defining various variables used
     private int countLoop = 0;
     private boolean should = false;
     private long timer = 0;
-    private long startime = 0;
+    private long starTime = 0;
+
     @Override
     public void init() {
-        telemetry.addData("Status", "Initialized");
-
-        /* eg: Initialize the hardware variables. Note that the strings used here as parameters
-         * to 'get' must correspond to the names assigned during the robot configuration
-         * step (using the FTC Robot Controller app on the phone).
-         */
+        //Getting the actual motors and binding them to the previously defined objects
         leftMotor  = hardwareMap.dcMotor.get("leftMotor");
         rightMotor = hardwareMap.dcMotor.get("rightMotor");
         elevator = hardwareMap.dcMotor.get("elevator");
         shooter = hardwareMap.dcMotor.get("shooter");
-
         poker = hardwareMap.servo.get("poker");
         pokerWheel = hardwareMap.servo.get("pokerWheel");
-
+        //Setting directions for the motors
         leftMotor.setDirection(DcMotor.Direction.FORWARD);
         rightMotor.setDirection(DcMotor.Direction.REVERSE);
         elevator.setDirection(DcMotor.Direction.FORWARD);
         shooter.setDirection(DcMotor.Direction.FORWARD);
-
+        //Setting default location for the servo
         pokerWheel.setPosition(0);
+        //Telling user that the initialization has been completed
         telemetry.addData("Status", "Initialized");
     }
 
@@ -91,12 +51,12 @@ public class OFFICIAL_PROGRAM extends OpMode
     @Override
     public void start() {
         runtime.reset();
+        //Moving poker out from previous position
         pokerWheel.setPosition(.5);
     }
 
     @Override
     public void loop() {
-        //make counter to determine th
         if (should) {
             countLoop++;
         }
@@ -109,12 +69,10 @@ public class OFFICIAL_PROGRAM extends OpMode
         if (gamepad1.dpad_up) {
             driveLeft = -1;
         }
-        else if (gamepad1.dpad_down)
-        {
+        else if (gamepad1.dpad_down) {
             driveLeft = 1;
         }
-        else
-        {
+        else {
             driveLeft = 0;
         }
         if (gamepad1.b) {
@@ -124,25 +82,22 @@ public class OFFICIAL_PROGRAM extends OpMode
             countLoop = 0;
         }
         if (gamepad2.b) {
-            timer = System.currentTimeMillis() - startime;
+            timer = System.currentTimeMillis() - starTime;
         }
         if (gamepad1.x) {
             poker.setPosition(.55);
             should = true;
-            startime = System.currentTimeMillis();
-
+            starTime = System.currentTimeMillis();
         }
         if (gamepad1.y) {
             poker.setPosition(.45);
             should = true;
-            startime = System.currentTimeMillis();
-
+            starTime = System.currentTimeMillis();
         }
         if (gamepad1.a) {
             poker.setPosition(.50);
         }
-        if (gamepad2.left_bumper)
-        {
+        if (gamepad2.left_bumper) {
             runElevator = -1;
         }
 
